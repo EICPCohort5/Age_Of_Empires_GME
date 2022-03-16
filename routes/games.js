af4472d496b600667cb95d7ef554be4e2a710d02
Alex Mazzarese <alex_mazzarese@tjx.com>
2022-03-16 11:44:10 -0400

    added to games.js under routes directory

--- routes/games.js
+++ routes/games.js
@@ -3,7 +3,19 @@ var router = express.Router();
 var {Games, Platforms, Genres, Publishers} = require('../orm/models')
 
 router.get('/', (req,res) => {
-    Games.findAll()
+    Games.findAll({
+        include: [{
+            model: Genres, as: 'GenreRef'
+        },
+        {
+            model: Publishers, as: 'PublisherRef'
+        },
+        {
+            model: Platforms, as: 'All_Platforms',
+            attributes: ['platformName']
+        }
+        ]
+    })
     .then(Games => {
         res.json(Games);
     })
@@ -11,6 +23,24 @@ router.get('/', (req,res) => {
         console.log(error);
         res.status(404).send(error);
     })
-})
+});
+
+router.get('/:id', (req,res) => {
+    Games.findByPk(req.params.id)
+    .then(games => {
+        res.json(games);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(404).send(error);
+    })
+});
+
+router.post('/', async (req, res) => {
+    console.log('req.body:', req.body);
+    let gameProto = req.body;
+
+    res.json(gameProto);
+});
 
 module.exports = router;
